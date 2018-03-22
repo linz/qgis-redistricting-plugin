ba2aa8fa9703ddbbdc81ce727d25c349f2b69126
Nyall Dawson <nyall.dawson@gmail.com>
2018-03-22 10:33:20 +1000

    More images

--- documentation/ui_design/mockups/mock_dock.py
+++ documentation/ui_design/mockups/mock_dock.py
@@ -1,4 +1,4 @@
-dock=QgsDockWidget('Redistricting')
+dock=QgsDockWidget('Redistricting - Scenario 1')
 dock_contents = QWidget()
 grid=QGridLayout(dock_contents)
 grid.setContentsMargins(0,0,0,0)
@@ -15,9 +15,25 @@ scenarios_tool_button.setToolTip('Scenarios')
 scenarios_tool_button.setIcon(QIcon('/home/nyall/dev/redistricting/images/scenarios.svg'))
 scenarios_tool_button.setPopupMode(QToolButton.InstantPopup)
 
+def select_current_scenario():
+    dlg = ScenarioSelectionDialog(iface.mainWindow())
+    dlg.exec_()        
+    
+def branch_scenario():
+    dlg = QgsNewNameDialog('Scenario 1', 'Scenario 1 Copy', parent=iface.mainWindow())
+    dlg.setWindowTitle('Branch to New Scenario')
+    dlg.setHintString('Enter name for new scenario')
+    dlg.show()
+    
 scenarios_menu = QMenu()
-store_scenario_action = QAction('Store Current Scenario...')
+switch_scenario_action = QAction('Switch to Existing Scenario...')
+switch_scenario_action.triggered.connect(select_current_scenario)
+scenarios_menu.addAction(switch_scenario_action)
+store_scenario_action = QAction('Branch to New Scenario...')
+store_scenario_action.triggered.connect(branch_scenario)
 scenarios_menu.addAction(store_scenario_action)
+import_scenario_action = QAction('Import Scenario from Database...')
+scenarios_menu.addAction(import_scenario_action)
 scenarios_tool_button.setMenu(scenarios_menu)
 
 dock_toolbar.addWidget(scenarios_tool_button)
@@ -52,9 +68,37 @@ help_action=QAction(QIcon('/home/nyall/dev/redistricting/images/help.svg'),'Help
 dock_toolbar.addAction(help_action)
 
 frame=QTextBrowser()
-frame.setHtml('<h3>Estimated Count</h3>')
+frame.setHtml('<h1>Scenario 1</h1><h3>Estimated Count</h3>')
 grid.addWidget(frame,1,0,1,1)
 
 dock.setWidget(dock_contents)
 
 iface.addDockWidget(Qt.RightDockWidgetArea, dock)
+
+class ScenarioSelectionDialog(QDialog):
+    
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        
+        self.setWindowTitle('Select Current Scenario')
+        
+        l = QVBoxLayout()
+        
+        search = QgsFilterLineEdit()
+        search.setShowSearchIcon(True)
+        search.setPlaceholderText('Search for scenario')
+        l.addWidget(search)
+        
+        list = QListWidget()
+        for i in range(10):
+            list.addItem( 'Scenario {}'.format(i))
+            
+        l.addWidget(list)
+        
+        bb = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
+        l.addWidget(bb)
+        bb.rejected.connect(self.reject)
+        bb.accepted.connect(self.accept)
+            
+        self.setLayout(l)
+        
