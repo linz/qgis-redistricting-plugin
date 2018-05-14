a3ae56f433fd7356ce2982c560652fe7e342d748
Nyall Dawson <nyall.dawson@gmail.com>
2018-05-14 12:04:38 +1000

    Start on scenario registry

---
+++ redistrict/linz/scenario_registry.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+"""LINZ Redistricting Plugin - Scenario registry
+
+.. note:: This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+"""
+
+__author__ = '(C) 2018 by Nyall Dawson'
+__date__ = '20/04/2018'
+__copyright__ = 'Copyright 2018, The QGIS Project'
+# This will get replaced with a git SHA1 when you do a git archive
+__revision__ = '$Format:%H$'
+
+from collections import OrderedDict
+from qgis.core import (QgsFeatureRequest,
+                       QgsExpression,
+                       QgsVectorLayer,
+                       NULL)
+
+
+class ScenarioRegistry():
+    """
+    A registry for handling available scenarios
+    """
+
+    def __init__(self, source_layer: QgsVectorLayer,
+                 id_field: str,
+                 name_field: str):
+        """
+        Constructor for ScenarioRegistry
+        :param source_layer: source layer for registry
+        :param id_field: name of scenario id field
+        :param name_field: name of scenario name field
+        """
+        self.source_layer = source_layer
+        self.id_field = id_field
+        self.id_field_index = source_layer.fields().lookupField(self.id_field)
+        assert self.id_field_index >= 0
+        self.name_field = name_field
+        self.name_field_index = source_layer.fields().lookupField(self.name_field)
+        assert self.name_field_index >= 0
+
+    def get_scenario_name(self, scenario):
+        """
+        Returns a user-friendly name corresponding to the given scenario
+        :param scenario: scenario id to get name for
+        """
+
+        # lookup matching feature
+        request = QgsFeatureRequest()
+        request.setFilterExpression(QgsExpression.createFieldEqualityExpression(self.id_field, scenario))
+        request.setFlags(QgsFeatureRequest.NoGeometry)
+        request.setSubsetOfAttributes([self.name_field_index])
+        f = next(self.source_layer.getFeatures(request))
+        return f[self.name_field_index]
+
+    def scenario_list(self):
+        """
+        Returns a complete list of available scenarios
+        """
+        request = QgsFeatureRequest()
+        request.setFlags(QgsFeatureRequest.NoGeometry)
+        request.setSubsetOfAttributes([self.id_field_index])
+
+        districts = [f[self.id_field_index]
+                     for f in self.source_layer.getFeatures(request)
+                     if f[self.id_field_index] != NULL]
+        # we want an ordered list of unique values!
+        d = OrderedDict()
+        for x in districts:
+            d[x] = True
+        return list(d.keys())
+
+    def scenario_titles(self):
+        """
+        Returns a dictionary of sorted scenario titles to corresponding scenario id
+        """
+        request = QgsFeatureRequest()
+        request.setFlags(QgsFeatureRequest.NoGeometry)
+        request.setSubsetOfAttributes([self.id_field_index, self.name_field_index])
+
+        districts = {f[self.name_field_index]: f[self.id_field_index]
+                     for f in self.source_layer.getFeatures(request)
+                     if f[self.id_field_index] != NULL}
+        result = OrderedDict()
+        for d in sorted(districts.keys()):
+            result[d] = districts[d]
+        return result
---
+++ redistrict/test/test_linz_scenario_registry.py
@@ -0,0 +1,106 @@
+# coding=utf-8
+"""LINZ Scenario Registry Test.
+
+.. note:: This program is free software; you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published by
+     the Free Software Foundation; either version 2 of the License, or
+     (at your option) any later version.
+
+"""
+
+__author__ = '(C) 2018 by Nyall Dawson'
+__date__ = '20/04/2018'
+__copyright__ = 'Copyright 2018, The QGIS Project'
+# This will get replaced with a git SHA1 when you do a git archive
+__revision__ = '$Format:%H$'
+
+import unittest
+from collections import OrderedDict
+from redistrict.linz.scenario_registry import ScenarioRegistry
+from qgis.core import (QgsVectorLayer,
+                       QgsFeature)
+
+
+def make_scenario_layer() -> QgsVectorLayer:
+    """
+    Makes a dummy scenario layer for testing
+    """
+    layer = QgsVectorLayer(
+        "NoGeometry?field=id:int&field=name:string",
+        "source", "memory")
+    f = QgsFeature()
+    f.setAttributes([1, "Scenario 1"])
+    f2 = QgsFeature()
+    f2.setAttributes([2, "scenario B"])
+    f3 = QgsFeature()
+    f3.setAttributes([3, "scenario 3"])
+    layer.dataProvider().addFeatures([f, f2, f3])
+    return layer
+
+
+class ScenarioRegistryTest(unittest.TestCase):
+    """Test ScenarioRegistry."""
+
+    def testScenarioRegistry(self):
+        """
+        Test a LinzDistrictRegistry
+        """
+        layer = make_scenario_layer()
+
+        reg = ScenarioRegistry(
+            source_layer=layer,
+            id_field='id',
+            name_field='name'
+        )
+        self.assertEqual(reg.source_layer, layer)
+        self.assertEqual(reg.id_field, 'id')
+        self.assertEqual(reg.id_field_index, 0)
+        self.assertEqual(reg.name_field, 'name')
+        self.assertEqual(reg.name_field_index, 1)
+
+    def testGetScenarioName(self):
+        """
+        Test retrieving scenario name
+        """
+        layer = make_scenario_layer()
+
+        reg = ScenarioRegistry(
+            source_layer=layer,
+            id_field='id',
+            name_field='name'
+        )
+        self.assertEqual(reg.get_scenario_name(1), "Scenario 1")
+        self.assertEqual(reg.get_scenario_name(2), "scenario B")
+        self.assertEqual(reg.get_scenario_name(3), "scenario 3")
+
+    def testGetScenarioList(self):
+        """
+        Test retrieving scenario list
+        """
+        layer = make_scenario_layer()
+
+        reg = ScenarioRegistry(
+            source_layer=layer,
+            id_field='id',
+            name_field='name'
+        )
+        self.assertEqual(reg.scenario_list(), [1, 2, 3])
+
+    def testGetScenarioTitles(self):
+        """
+        Test retrieving scenario titles
+        """
+        layer = make_scenario_layer()
+
+        reg = ScenarioRegistry(
+            source_layer=layer,
+            id_field='id',
+            name_field='name'
+        )
+        self.assertEqual(reg.scenario_titles(), OrderedDict([('Scenario 1', 1), ('scenario 3', 3), ('scenario B', 2)]))
+
+
+if __name__ == "__main__":
+    suite = unittest.makeSuite(ScenarioRegistry)
+    runner = unittest.TextTestRunner(verbosity=2)
+    runner.run(suite)
