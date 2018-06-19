aa3da17a86c8d5e6a833708d5454badf2245deb9
Nyall Dawson <nyall.dawson@gmail.com>
2018-06-20 08:26:59 +1000

    Test that queue changes features in electorate layer

--- redistrict/test/test_linz_electorate_changes_queue.py
+++ redistrict/test/test_linz_electorate_changes_queue.py
@@ -40,7 +40,8 @@ class LINZElectorateQueueTest(unittest.TestCase):
         d.setGeometry(QgsGeometry.fromRect(QgsRectangle(5, 0, 10, 5)))
         d2 = QgsFeature()
         d2.setAttributes(["test2", NULL, 11112, 22, 23, 0, 'y'])
-        d2.setGeometry(QgsGeometry.unaryUnion([QgsGeometry.fromRect(QgsRectangle(5, 5, 10, 10)), QgsGeometry.fromRect(QgsRectangle(0, 10, 10, 15))]))
+        d2.setGeometry(QgsGeometry.unaryUnion(
+            [QgsGeometry.fromRect(QgsRectangle(5, 5, 10, 10)), QgsGeometry.fromRect(QgsRectangle(0, 10, 10, 15))]))
         d3 = QgsFeature()
         d3.setAttributes(["test3", NULL, 11113, 32, 33, 1, 'z'])
         d3.setGeometry(QgsGeometry.fromRect(QgsRectangle(0, 5, 5, 10)))
@@ -53,6 +54,11 @@ class LINZElectorateQueueTest(unittest.TestCase):
         self.assertTrue(success)
 
         queue = ElectorateEditQueue(electorate_layer=district_layer)
+
+        queue.push({d.id(): {0: 'xtest1', 2: 21111}}, {d.id(): QgsGeometry.fromRect(QgsRectangle(115, 0, 110, 5))})
+        self.assertEqual(district_layer.getFeature(d.id()).attributes(), ['xtest1', NULL, 21111, 12, 13, 1, 'x'])
+        self.assertEqual(district_layer.getFeature(d.id()).geometry().asWkt(),
+                         'Polygon ((110 0, 115 0, 115 5, 110 5, 110 0))')
 
 
 if __name__ == "__main__":
